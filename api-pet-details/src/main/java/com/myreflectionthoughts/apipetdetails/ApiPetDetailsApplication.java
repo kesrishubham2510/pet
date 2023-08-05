028e6e24bdfca38cd3215f4b24fa4cf097622855
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-08-05 15:14:30 +0530

    Initial setup

---
+++ api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/ApiPetDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.myreflectionthoughts.apipetdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPetDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPetDetailsApplication.class, args);
+	}
+
+}
