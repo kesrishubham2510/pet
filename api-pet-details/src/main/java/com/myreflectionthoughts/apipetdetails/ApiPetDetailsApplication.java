f7859fe0d798b6154fd4b19ef738751a21ad5801
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-08-06 18:22:17 +0530

    Added annotation to scan the complete package for configurations

--- api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/ApiPetDetailsApplication.java
+++ api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/ApiPetDetailsApplication.java
@@ -2,12 +2,13 @@ package com.myreflectionthoughts.apipetdetails;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.myreflectionthoughts.apipetdetails.*"})
 public class ApiPetDetailsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiPetDetailsApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiPetDetailsApplication.class, args);
+    }
 }
