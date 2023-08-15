8e41f13447bf90291c4f23344f3e15c76f43fa08
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-08-15 10:55:29 +0530

    * Added api-master-details.

---
+++ api-master-details/src/main/java/com/myreflectionthoughts/apimasterdetails/ApiMasterDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.myreflectionthoughts.apimasterdetails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMasterDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMasterDetailsApplication.class, args);
+	}
+
+}
---
+++ api-master-details/src/test/java/com/myreflectionthoughts/apimasterdetails/ApiMasterDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myreflectionthoughts.apimasterdetails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMasterDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
