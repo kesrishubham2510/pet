529b6f5e61383f876925a0a920ac370908ab261b
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-08-18 14:25:43 +0530

    * Removed unused import

--- api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/configuration/MongoConfig.java
+++ api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/configuration/MongoConfig.java
@@ -1,7 +1,6 @@
 package com.myreflectionthoughts.apipetdetails.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
 @Configuration
 public class MongoConfig {
