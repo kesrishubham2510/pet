555a462d257d8b59864a904a68f10e741fef3446
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-08-20 15:42:51 +0530

    SetUp OpenAPI dependencies

--- api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/ApiPetDetailsApplication.java
+++ api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/ApiPetDetailsApplication.java
@@ -1,5 +1,7 @@
 package com.myreflectionthoughts.apipetdetails;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -8,6 +10,8 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 @SpringBootApplication
 @EnableReactiveMongoRepositories
 @ComponentScan(basePackages = {"com.myreflectionthoughts.apipetdetails.*"})
+@OpenAPIDefinition(info = @Info(title = "Pet API", version = "1.0", description = "Documentation for Pet Rest Service"))
+
 public class ApiPetDetailsApplication {
 
     public static void main(String[] args) {
---
+++ api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/configuration/SwaggerConfig.java
@@ -0,0 +1,7 @@
+package com.myreflectionthoughts.apipetdetails.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+}
