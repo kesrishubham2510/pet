a08724f8a67e32e6cfebee0c7fa97e46bf2551d7
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-08-25 21:49:19 +0530

    * Removed unused ModelMapper bean

--- api-master-details/src/main/java/com/myreflectionthoughts/apimasterdetails/gateway/dataprovider/component/UtilsComponent.java
+++ api-master-details/src/main/java/com/myreflectionthoughts/apimasterdetails/gateway/dataprovider/component/UtilsComponent.java
@@ -1,17 +1,11 @@
 package com.myreflectionthoughts.apimasterdetails.gateway.dataprovider.component;
 
 import com.myreflectionthoughts.apimasterdetails.gateway.dataprovider.utility.MappingUtility;
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UtilsComponent {
-
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 
     @Bean
     public MappingUtility mappingUtility() { return new MappingUtility(); }
