1aaf071b26e71777bedf09f24623962a2f653ff1
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-08-27 17:04:38 +0530

    * Added method declaration to retrieve pets based on masterId

--- api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/gateway/dataprovider/repository/PetRepository.java
+++ api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/gateway/dataprovider/repository/PetRepository.java
@@ -1,9 +1,13 @@
 package com.myreflectionthoughts.apipetdetails.gateway.dataprovider.repository;
 
 import com.myreflectionthoughts.apipetdetails.core.entity.Pet;
+import com.myreflectionthoughts.library.dto.response.PetDTO;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Flux;
 
 @Repository
 public interface PetRepository extends ReactiveMongoRepository<Pet, String> {
+
+    public Flux<PetDTO> findAllByMasterId(String masterId);
 }
