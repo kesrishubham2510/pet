99627110c31e7d8e355fe4ae6d65ba906f03627b
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-08-27 15:25:20 +0530

    * Added API_QUALIFIER and response messages

--- api-gateway/src/main/java/com/myreflectionthoughts/apigateway/core/constant/ServiceConstant.java
+++ api-gateway/src/main/java/com/myreflectionthoughts/apigateway/core/constant/ServiceConstant.java
@@ -4,4 +4,7 @@ public class ServiceConstant {
 
     public static final String masterServiceQualifier = "masterService";
     public static final String petServiceQualifier = "petService";
+    public  static final String API_QUALIFIER = "/api-pet";
+    public static final String API_RESPONSE_200_MESSAGE = "Operation successful";
+    public static final String API_RESPONSE_400_MESSAGE = "Something is wrong, please check message for details";
 }
