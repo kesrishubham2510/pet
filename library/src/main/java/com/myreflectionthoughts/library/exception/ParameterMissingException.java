19667c53a134d26f648b39d97327308d3fe6d0af
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-09-01 12:45:46 +0530

    * Added Exception to handle empty or null parameter exceptions

---
+++ library/src/main/java/com/myreflectionthoughts/library/exception/ParameterMissingException.java
@@ -0,0 +1,7 @@
+package com.myreflectionthoughts.library.exception;
+
+public class ParameterMissingException extends RuntimeException {
+    public ParameterMissingException(String message) {
+        super(message);
+    }
+}
