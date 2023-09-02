e139726a076c66cad0b10dc8c96cf69d744f1844
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-09-02 12:16:02 +0530

    * Added tests to validate the AddMaster DTO Validation scenarios

---
+++ api-master-details/src/test/java/com/myreflectionthoughts/apimasterdetails/gateway/dataprovider/endtoend/AddMasterBeanValidationTest.java
@@ -0,0 +1,119 @@
+/*
+    BDD test to validate the InputDataExceptionScenarios
+ */
+package com.myreflectionthoughts.apimasterdetails.gateway.dataprovider.endtoend;
+
+import com.myreflectionthoughts.apimasterdetails.core.constant.ServiceConstants;
+import com.myreflectionthoughts.apimasterdetails.gateway.dataprovider.TestDataGenerator;
+import com.myreflectionthoughts.library.dto.request.AddMasterDTO;
+import com.myreflectionthoughts.library.dto.response.ExceptionResponse;
+import com.myreflectionthoughts.library.exception.InputDataException;
+import com.myreflectionthoughts.library.utils.ValidationUtils;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@SpringBootTest
+public class AddMasterBeanValidationTest extends TestSetup{
+
+    @Test
+    void testAddMaster_should_throw_InputDataException_for_empty_email(){
+
+        AddMasterDTO requestPayload = TestDataGenerator.generateAddMasterDTO();
+        requestPayload.setEmail("");
+
+        webTestClient.post()
+                .uri(String.format("%s/add", ServiceConstants.API_QUALIFIER))
+                .bodyValue(requestPayload)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(ExceptionResponse.class)
+                .consumeWith(exceptionResponse->{
+                    assertEquals(InputDataException.class.getSimpleName(), exceptionResponse.getResponseBody().getError());
+                    assertEquals("Email ID is required, it can't null or empty or whitespaces", exceptionResponse.getResponseBody().getErrorMessage());
+                });
+
+
+    }
+
+    @Test
+    void testAddMaster_should_throw_InputDataException_for_emailDomain(){
+
+        AddMasterDTO requestPayload = TestDataGenerator.generateAddMasterDTO();
+        requestPayload.setEmail("newUser@reddit.com");
+
+        webTestClient.post()
+                .uri(String.format("%s/add", ServiceConstants.API_QUALIFIER))
+                .bodyValue(requestPayload)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(ExceptionResponse.class)
+                .consumeWith(exceptionResponse->{
+                    assertEquals(InputDataException.class.getSimpleName(), exceptionResponse.getResponseBody().getError());
+                    assertEquals("Email must contain one of these domains [gmail.com, facebook.com, yopmail.com]", exceptionResponse.getResponseBody().getErrorMessage());
+                });
+    }
+
+    @Test
+    void testAddMaster_should_throw_InputDataException_for_null_email(){
+
+        AddMasterDTO requestPayload = TestDataGenerator.generateAddMasterDTO();
+        requestPayload.setEmail(null);
+
+        webTestClient.post()
+                .uri(String.format("%s/add", ServiceConstants.API_QUALIFIER))
+                .bodyValue(requestPayload)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(ExceptionResponse.class)
+                .consumeWith(exceptionResponse->{
+                    assertEquals(InputDataException.class.getSimpleName(), exceptionResponse.getResponseBody().getError());
+                    assertEquals("Email ID is required, it can't null or empty or whitespaces", exceptionResponse.getResponseBody().getErrorMessage());
+                });
+
+
+    }
+
+    @Test
+    void testAddMaster_should_throw_InputDataException_for_passwordLength(){
+
+        AddMasterDTO requestPayload = TestDataGenerator.generateAddMasterDTO();
+        requestPayload.setPassword("news");
+
+        webTestClient.post()
+                .uri(String.format("%s/add", ServiceConstants.API_QUALIFIER))
+                .bodyValue(requestPayload)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(ExceptionResponse.class)
+                .consumeWith(exceptionResponse->{
+                    assertEquals(InputDataException.class.getSimpleName(), exceptionResponse.getResponseBody().getError());
+                    assertEquals(String.format("Password should be atleast %s characters long", ValidationUtils.passwordLength), exceptionResponse.getResponseBody().getErrorMessage());
+                });
+    }
+    @Test
+    void testAddMaster_should_throw_InputDataException_for_age(){
+
+        AddMasterDTO requestPayload = TestDataGenerator.generateAddMasterDTO();
+        requestPayload.setAge(-1);
+
+        webTestClient.post()
+                .uri(String.format("%s/add", ServiceConstants.API_QUALIFIER))
+                .bodyValue(requestPayload)
+                .exchange()
+                .expectStatus()
+                .isBadRequest()
+                .expectBody(ExceptionResponse.class)
+                .consumeWith(exceptionResponse->{
+                    assertEquals(InputDataException.class.getSimpleName(), exceptionResponse.getResponseBody().getError());
+                    assertEquals(String.format("Please provide a Valid age should be atleast %s years old",ValidationUtils.ageThreshold), exceptionResponse.getResponseBody().getErrorMessage());
+                });
+    }
+
+}
