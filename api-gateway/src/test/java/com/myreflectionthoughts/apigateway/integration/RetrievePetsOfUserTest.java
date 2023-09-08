fbad59d7d043cf9a7bbd3a2ae129f07b36502777
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-09-08 23:21:12 +0530

    * Changed wireMock dependency, as it was compatible with jdk 11 * Added integration tests for RetrievedPetsOfUserUseCase

---
+++ api-gateway/src/test/java/com/myreflectionthoughts/apigateway/integration/RetrievePetsOfUserTest.java
@@ -0,0 +1,54 @@
+package com.myreflectionthoughts.apigateway.integration;
+
+
+import com.github.tomakehurst.wiremock.client.WireMock;
+import com.myreflectionthoughts.apigateway.core.constant.ServiceConstant;
+import com.myreflectionthoughts.library.dto.response.PetDTO;
+import org.junit.jupiter.api.Test;
+
+public class RetrievePetsOfUserTest extends TestSetup{
+
+    @Test
+    void testRetrieveUserPets(){
+
+        wireMockServer.stubFor(
+                WireMock
+                        .get(String.format("%s/get/pets/%s", ServiceConstant.petServiceAPIQualifier, ServiceConstant.VALID_MASTER_ID))
+                        .willReturn(WireMock.aResponse()
+                                .withStatus(200)
+                                .withHeader("Content-Type","application/x-ndjson")
+                                .withBodyFile("RetrievedPetsOfUser.json"))
+        );
+
+        webTestClient.get()
+                     .uri(String.format("%s/get/pets/%s", ServiceConstant.API_QUALIFIER, ServiceConstant.VALID_MASTER_ID))
+                     .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(PetDTO.class)
+                .hasSize(1);
+    }
+
+    @Test
+    void testRetrieveUserPets_EmptyPetList(){
+
+        wireMockServer.stubFor(
+                WireMock.get(String.format("%s/get/pets/%s", ServiceConstant.petServiceAPIQualifier, ServiceConstant.VALID_MASTER_ID))
+                        .willReturn(
+                                WireMock
+                                        .aResponse()
+                                        .withStatus(200)
+                                        .withHeader("Content-Type","application/x-ndjson")
+                                        .withBodyFile("RetrievedPetsOfUser_EmptyList.json"))
+        );
+
+        webTestClient.get()
+                .uri(String.format("%s/get/pets/%s", ServiceConstant.API_QUALIFIER, ServiceConstant.VALID_MASTER_ID))
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .expectBodyList(PetDTO.class)
+                .hasSize(0);
+    }
+
+}
