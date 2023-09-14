ce586e22c809a15eb729d6cc5dbc4532081a83b2
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-09-14 22:16:24 +0530

    * Added exceptionMessage field

--- library/src/main/java/com/myreflectionthoughts/library/dto/logs/ExceptionLog.java
+++ library/src/main/java/com/myreflectionthoughts/library/dto/logs/ExceptionLog.java
@@ -2,19 +2,30 @@ package com.myreflectionthoughts.library.dto.logs;
 
 public class ExceptionLog extends CustomLog {
     private final String exception;
+    private final String exceptionMessage;
 
-    public ExceptionLog(CustomLog customLog, String exception) {
+    public ExceptionLog(CustomLog customLog, String exception, String exceptionMessage) {
         super(customLog);
         this.exception = exception;
+        this.exceptionMessage = exceptionMessage;
     }
 
     public String getException() {
         return exception;
     }
 
+    public String getExceptionMessage(){
+        return exceptionMessage;
+    }
+
     @Override
     public String toString(){
         String log = super.toString();
-        return new StringBuilder(log.substring(0, log.length()-1)).append(", exception:- ").append(this.exception).append(" )").toString();
+        return new StringBuilder(log.substring(0, log.length()-1))
+                .append(", exception:- ")
+                .append(this.exception)
+                .append(", exceptionMessage:- ")
+                .append(this.exceptionMessage)
+                .append(" )").toString();
     }
 }
