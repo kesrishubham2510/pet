a1703b29160ee6df38b4fae6168df347bec607d8
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-09-16 23:53:30 +0530

    * Added Impl for ILogger, IEntryLogger & IExitLogger

---
+++ library/src/main/java/com/myreflectionthoughts/library/dto/logs/LoggerUtility.java
@@ -0,0 +1,29 @@
+package com.myreflectionthoughts.library.dto.logs;
+
+import com.myreflectionthoughts.library.contract.IEntryLogger;
+import com.myreflectionthoughts.library.contract.IExitLogger;
+import com.myreflectionthoughts.library.contract.ILogger;
+
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoggerUtility implements ILogger, IEntryLogger, IExitLogger {
+
+    // customLog is to be sent to kafka for storage
+
+    @Override
+    public void log(Logger logger, CustomLog customLog, Level logLevel) {
+        logger.log(Objects.requireNonNullElse(logLevel, Level.WARNING), customLog.message);
+    }
+
+    @Override
+    public void logEntry(Logger logger, String message) {
+        logger.log(Level.INFO, message);
+    }
+
+    @Override
+    public void logExit(Logger logger, String message) {
+        logger.log(Level.INFO, message);
+    }
+}
