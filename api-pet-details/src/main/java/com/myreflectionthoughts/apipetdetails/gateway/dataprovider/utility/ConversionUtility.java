92c9c216fcb589d8a4d6380d728489fc1fbec9be
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-09-17 16:05:25 +0530

    * Added logs

--- api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/gateway/dataprovider/utility/ConversionUtility.java
+++ api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/gateway/dataprovider/utility/ConversionUtility.java
@@ -1,34 +1,44 @@
 package com.myreflectionthoughts.apipetdetails.gateway.dataprovider.utility;
 
+import com.myreflectionthoughts.apipetdetails.core.entity.Pet;
 import com.myreflectionthoughts.apipetdetails.core.enums.Category;
 import com.myreflectionthoughts.apipetdetails.core.enums.ClinicCardStatus;
 import com.myreflectionthoughts.apipetdetails.core.enums.Gender;
 import org.modelmapper.AbstractConverter;
 import org.modelmapper.Converter;
 
+import java.util.logging.Logger;
+
 public class ConversionUtility {
 
     private final ValidationUtility validationUtility;
+    private final Logger logger;
+
 
     public ConversionUtility(ValidationUtility validationUtility) {
         this.validationUtility = validationUtility;
+        logger = Logger.getLogger(ConversionUtility.class.getName());
     }
 
     protected Converter<Gender, String> gender_To_stringConverter = new AbstractConverter<>() {
         @Override
         protected String convert(Gender source) {
+            Utility.loggerUtility.logEntry(logger, "Initialised Gender --> String converter...");
             return source.toString();
         }
+
     };
     protected Converter<Category, String> category_To_stringConverter = new AbstractConverter<>() {
         @Override
         protected String convert(Category source) {
+            Utility.loggerUtility.logEntry(logger, "Initialised Category --> String converter...");
             return source.toString();
         }
     };
     protected Converter<ClinicCardStatus, String> clinicCardStatus_To_stringConverter = new AbstractConverter<>() {
         @Override
         protected String convert(ClinicCardStatus source) {
+            Utility.loggerUtility.logEntry(logger, "Initialised ClinicCardStatus --> String converter...");
             return source.toString();
         }
     };
@@ -36,12 +46,14 @@ public class ConversionUtility {
     protected Converter<String, Category> string_To_categoryConverter = new AbstractConverter<>() {
         @Override
         protected Category convert(String source) {
+            Utility.loggerUtility.logEntry(logger, "Initialised String --> Category converter...");
             return validationUtility.validateCategory(source);
         }
     };
     protected Converter<String, Gender> string_To_genderConverter = new AbstractConverter<>() {
         @Override
         protected Gender convert(String source) {
+            Utility.loggerUtility.logEntry(logger, "Initialised String --> Gender converter...");
             return validationUtility.validateGender(source);
         }
     };
@@ -49,6 +61,7 @@ public class ConversionUtility {
     protected Converter<String,ClinicCardStatus> string_To_clinicCardStatusConverter = new AbstractConverter<>() {
         @Override
         protected ClinicCardStatus convert(String source) {
+            Utility.loggerUtility.logEntry(logger, "Initialised String --> ClinicCardStatus converter...");
             return validationUtility.validateClinicCardStatus(source);
         }
     };
