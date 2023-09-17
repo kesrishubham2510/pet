f7dfa6ca65572cc59efb76570abc41088e34ee1f
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-09-17 10:58:59 +0530

    * Added Basic Utility with LoggerUtility

---
+++ api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/gateway/dataprovider/utility/Utility.java
@@ -0,0 +1,11 @@
+package com.myreflectionthoughts.apipetdetails.gateway.dataprovider.utility;
+
+import com.myreflectionthoughts.library.dto.logs.LoggerUtility;
+
+public class Utility {
+    protected final LoggerUtility loggerUtility;
+
+    public Utility(LoggerUtility loggerUtility) {
+        this.loggerUtility = loggerUtility;
+    }
+}
