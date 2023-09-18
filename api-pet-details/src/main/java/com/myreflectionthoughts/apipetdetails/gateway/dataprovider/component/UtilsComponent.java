1707012b56ad9eb8c5b75dc14c29487f3ca63818
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-09-18 16:47:23 +0530

    * Added loggers

--- api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/gateway/dataprovider/component/UtilsComponent.java
+++ api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/gateway/dataprovider/component/UtilsComponent.java
@@ -1,5 +1,6 @@
 package com.myreflectionthoughts.apipetdetails.gateway.dataprovider.component;
 
+import com.myreflectionthoughts.apipetdetails.core.utils.LogUtility;
 import com.myreflectionthoughts.apipetdetails.gateway.dataprovider.utility.ConversionUtility;
 import com.myreflectionthoughts.apipetdetails.gateway.dataprovider.utility.ExceptionUtility;
 import com.myreflectionthoughts.apipetdetails.gateway.dataprovider.utility.MappingUtility;
@@ -8,21 +9,33 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Component
 public class UtilsComponent {
 
+    private final Logger logger;
+
+    public UtilsComponent(){
+        logger = Logger.getLogger(UtilsComponent.class.getName());
+    }
+
     @Bean
     public ModelMapper modelMapper() {
+        LogUtility.loggerUtility.log(logger, "Initialising the ModelMapper bean" , Level.FINE);
         return new ModelMapper();
     }
 
     @Bean
     public ConversionUtility conversionUtility() {
+        LogUtility.loggerUtility.log(logger, "Initialising the ConversionUtility bean" , Level.FINE);
         return new ConversionUtility(validationUtility());
     }
 
     @Bean
     public MappingUtility mappingUtility(ModelMapper modelMapper, ConversionUtility conversionUtility) {
+        LogUtility.loggerUtility.log(logger, "Initialising the MappingUtility bean" , Level.FINE);
         return new MappingUtility(conversionUtility, modelMapper);
     }
 
