cd20f46c631ca4e24faccb3d357b0aebaf95858a
kesrishubham2510 <shubhamkeshari90@gmail.com>
2023-09-18 14:57:05 +0530

    * Added logs for handler operations

--- api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/gateway/handler/GetPetRequestHandler.java
+++ api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/gateway/handler/GetPetRequestHandler.java
@@ -1,23 +1,36 @@
 package com.myreflectionthoughts.apipetdetails.gateway.handler;
 
 import com.myreflectionthoughts.apipetdetails.core.usecase.ReadPetDetailsUseCase;
+import com.myreflectionthoughts.apipetdetails.core.utils.LogUtility;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Service
 public class GetPetRequestHandler extends Handler{
     private final ReadPetDetailsUseCase readPetDetailsUseCase;
+    private final Logger logger;
 
     public GetPetRequestHandler(ReadPetDetailsUseCase readPetDetailsUseCase) {
         this.readPetDetailsUseCase = readPetDetailsUseCase;
+        this.logger = Logger.getLogger(GetPetRequestHandler.class.getName());
     }
 
     public Mono<ServerResponse> getPet(ServerRequest serverRequest){
         String petId = serverRequest.pathVariable("petId");
-        return readPetDetailsUseCase.getDetails(Mono.just(petId))
+
+        LogUtility.loggerUtility.logEntry(logger, "Initiating get pet info request processing...");
+
+        return readPetDetailsUseCase
+                .getDetails(Mono.just(petId))
+                .doOnNext(retrievedPet-> LogUtility.loggerUtility.log(logger, "Pet:- "+retrievedPet.getId()+" info retrieved successfully", Level.INFO))
+                .doOnNext(retrievedPet-> LogUtility.loggerUtility.log(logger, "Response:- "+retrievedPet, Level.FINE))
                 .flatMap(petDetailResponse-> ServerResponse.ok().bodyValue(petDetailResponse))
+                .doOnNext(generatedResponse-> LogUtility.loggerUtility.log(logger, "pet info retrieval request processed with successfull response generation", Level.INFO))
                 .onErrorResume(exceptionMapper);
     }
 }
--- api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/gateway/handler/UpdatePetRequestHandler.java
+++ api-pet-details/src/main/java/com/myreflectionthoughts/apipetdetails/gateway/handler/UpdatePetRequestHandler.java
@@ -1,25 +1,38 @@
 package com.myreflectionthoughts.apipetdetails.gateway.handler;
 
 import com.myreflectionthoughts.apipetdetails.core.usecase.UpdatePetDetailsUseCase;
+import com.myreflectionthoughts.apipetdetails.core.utils.LogUtility;
 import com.myreflectionthoughts.library.dto.request.UpdatePetDTO;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @Service
 public class UpdatePetRequestHandler extends Handler{
 
     private final UpdatePetDetailsUseCase updatePetDetailsUseCase;
+    private final Logger logger;
 
     public UpdatePetRequestHandler(UpdatePetDetailsUseCase updatePetDetailsUseCase) {
         this.updatePetDetailsUseCase = updatePetDetailsUseCase;
+        logger = Logger.getLogger(UpdatePetRequestHandler.class.getName());
     }
 
     public Mono<ServerResponse> updatePet(ServerRequest serverRequest){
         Mono<UpdatePetDTO> updatePetDTOMono = serverRequest.bodyToMono(UpdatePetDTO.class);
-        return updatePetDetailsUseCase.updatePetDetails(updatePetDTOMono)
+
+        LogUtility.loggerUtility.logEntry(logger, "Initiating update pet request processing...");
+
+        return updatePetDetailsUseCase
+                .updatePetDetails(updatePetDTOMono)
+                .doOnNext(updatePet-> LogUtility.loggerUtility.log(logger, "Pet:- "+updatePet.getId()+" updated successfully...", Level.INFO))
+                .doOnNext(updatePet-> LogUtility.loggerUtility.log(logger, "Response:- "+updatePet, Level.FINE))
                 .flatMap(updatedPetResponse-> ServerResponse.ok().bodyValue(updatedPetResponse))
+                .doOnNext(generatedResponse-> LogUtility.loggerUtility.log(logger, "Update pet request processed with successful response generation", Level.INFO))
                 .onErrorResume(exceptionMapper);
     }
 }
